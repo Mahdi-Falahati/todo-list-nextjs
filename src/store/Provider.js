5309457022bb8e76a5a3d195b5f42873a020fbd3
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-07-29 12:15:26 -0700

    create redux provider

---
+++ src/store/Provider.js
@@ -0,0 +1,8 @@
+"use client"
+
+import { Provider } from 'react-redux';
+import store from './Store';
+
+export default function ReduxProvider ({children}){
+    return <Provider store={store}>{children}</Provider>
+}
