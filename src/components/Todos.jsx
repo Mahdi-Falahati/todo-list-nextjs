cf4bc0d93867c6245f563169c228fe74e93106d8
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-07-30 10:57:43 -0700

    center element

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -2,12 +2,13 @@
 
 import FormDialog from "./FormDialog";
 import Output from "./Output";
+import { CenterElement } from "./customComponent";
 
 export default function Todos() {
   return (
-    <>
+    <CenterElement sx={{ flexDirection: "column" }}>
       <FormDialog />
       <Output />
-    </>
+    </CenterElement>
   );
 }
