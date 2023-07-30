fdfaab53fd2dd65638363d53ccbcbc1eab07a80d
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-07-30 13:52:55 -0700

    remove function is defiend but never used

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,6 @@
-import Todo from "@/components/Todo";
+import Todo from "@/components/Todos";
 
 export default function Home() {
-  const handleAddTodo = (data) => {
-    console.log('Todo:', data.todo);
-    // Perform your logic to add the todo
-  };
   return (
     <main>
       <Todo/>
