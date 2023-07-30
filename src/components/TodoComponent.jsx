1cb61b261e0624dbfa04260b5d72f04430ff3a2e
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-07-30 12:15:27 -0700

    add modal to todo for description

--- src/components/TodoComponent.jsx
+++ src/components/TodoComponent.jsx
@@ -1,41 +1,63 @@
 "use client";
-import styled from "@emotion/styled";
-import { Box, Button, Stack } from "@mui/material";
-
+import { Box, ButtonGroup, Stack } from "@mui/material";
 import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn";
 import PreviewIcon from "@mui/icons-material/Preview";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { useState } from "react";
+import { IconBtn, TodoElement } from "./customComponent";
 
-export default function TodoComponent() {
+export default function TodoComponent({ title, description }) {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <TodoElement sx={{ width: { xs: "200px", sm: "350px", md: "400px" } }}>
-      <p>helo</p>
+      <p>{title}</p>
       <Box>
-        <IconButton>
-          <AssignmentTurnedInIcon />
-        </IconButton>
-        <IconButton>
-          <PreviewIcon />
-        </IconButton>
+        <ButtonGroup
+          color="secondary"
+          aria-label="icon button"
+          orientation="horizontal"
+          variant="text"
+        >
+          <IconBtn onClick={handleOpen}>
+            <PreviewIcon sx={{ fontSize: "30px", color: "red" }} />
+          </IconBtn>
+          <IconBtn>
+            <AssignmentTurnedInIcon sx={{ fontSize: "29px", color: "green" }} />
+          </IconBtn>
+        </ButtonGroup>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              {title}
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              {description}
+            </Typography>
+          </Box>
+        </Modal>
       </Box>
     </TodoElement>
   );
 }
 
-const TodoElement = styled(Stack)(() => ({
-  display: "flex",
-  justifyContent: "space-between",
-  padding: "0px 25px",
-  flexDirection: "row",
-  alignItems: "center",
-  borderRadius: "15px",
-  boxShadow: "inset 2px 2px 5px 0px #999",
-  height: "45px",
-}));
-
-const IconButton = styled(Button)(() => ({
-  margin: "0px",
-  padding: "0px",
-  border: "none",
-  outline: "none",
-  curser: "pointer",
-}));
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
