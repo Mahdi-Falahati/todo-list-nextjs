03e9fba0b7e1878a72213eb95265ab8590d148fe
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-08-03 03:42:29 -0700

    onclick test for todo btn

--- src/test/formdialog.test.js
+++ src/test/formdialog.test.js
@@ -1,19 +1,41 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { AddTodoButton } from "../components/customComponent";
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
+import { TextField } from "@mui/material";
 
-test("renders Add Todo button", () => {
-  render(
-    <AddTodoButton
-      autoFocus
-      color="inherit"
-      type="submit"
-      variant="contained"
-    >
-      Add Todo
-    </AddTodoButton>
-  );
+describe("<TextField />", () => {
+  test("renders the label correctly", () => {
+    render(<TextField label="Description" />);
+    expect(screen.getByLabelText("Description")).toBeInTheDocument();
+  });
+});
 
-  const addButton = screen.getByRole("button", { name: /add todo/i });
-  expect(addButton).toBeInTheDocument();
+describe("<AddTodoButton/>", () => {
+  test("renders Add Todo button", () => {
+    render(
+      <AddTodoButton
+        autoFocus
+        color="inherit"
+        type="submit"
+        variant="contained"
+      >
+        Add Todo
+      </AddTodoButton>
+    );
+
+    const addButton = screen.getByRole("button", { name: /add todo/i });
+    expect(addButton).toBeInTheDocument();
+  });
+
+  test("should call the onClick handler when clicked", () => {
+    const onClickMock = jest.fn();
+    const { getByText } = render(
+      <AddTodoButton onClick={onClickMock}>Add Todo</AddTodoButton>
+    );
+
+    const addButton = getByText("Add Todo");
+    fireEvent.click(addButton);
+
+    expect(onClickMock).toHaveBeenCalled();
+  });
 });
