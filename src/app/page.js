23da7262c636630392a8c88ed2900d6c5d16d95a
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-08-22 11:28:45 -0700

    add dark mode

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,36 @@
+"use client";
 import Todo from "@/components/Todos";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { useState } from "react";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import { IconButton } from "@mui/material";
+import NightsStayIcon from '@mui/icons-material/NightsStay';
 
 export default function Home() {
+  const [toggleDark, settoggleDark] = useState(false);
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: toggleDark ? "dark" : "light",
+    },
+  });
+
   return (
-    <main>
-      <Todo/>
-    </main>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <main>
+        <IconButton
+          aria-label="dark-mode"
+          color="secondary"
+          onClick={() => settoggleDark(!toggleDark)}
+        >
+          <LightModeIcon />
+          <NightsStayIcon />
+        </IconButton>
+
+        <Todo />
+      </main>
+    </ThemeProvider>
   );
 }
